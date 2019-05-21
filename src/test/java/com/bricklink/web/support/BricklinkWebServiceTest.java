224c4769afc3bac83d2b1974648e8a2c778a6f0e
tvattima <github@vattima.com>
2019-05-20 20:10:39 -0400

    Introduces imgscalr for scaling thumbnails of primary inventory photo

--- src/test/java/com/bricklink/web/support/BricklinkWebServiceTest.java
+++ src/test/java/com/bricklink/web/support/BricklinkWebServiceTest.java
@@ -4,6 +4,7 @@ import com.bricklink.web.configuration.BricklinkWebProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.springframework.util.StopWatch;
 
 import java.nio.file.Paths;
 
@@ -13,15 +14,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BricklinkWebServiceTest {
     @Test
     public void canAuthenticate() throws Exception {
-        BricklinkWebProperties properties = new BricklinkWebProperties();
-        properties.setClientConfigDir(Paths.get("C:\\Users\\tvatt\\.credentials\\bricklink"));
-        properties.setClientConfigFile(Paths.get("bricklink-web.json"));
-        ObjectMapper mapper = new ObjectMapper();
+        BricklinkSession bricklinkSession = null;
+        StopWatch timer = new StopWatch();
+        timer.start();
+        try {
+            BricklinkWebProperties properties = new BricklinkWebProperties();
+            properties.setClientConfigDir(Paths.get("C:\\Users\\tvatt\\.credentials\\bricklink"));
+            properties.setClientConfigFile(Paths.get("bricklink-web.json"));
+            ObjectMapper mapper = new ObjectMapper();
 
-        BricklinkWebService bricklinkWebService = new BricklinkWebService(properties, mapper);
-        BricklinkSession bricklinkSession = bricklinkWebService.authenticate();
-        bricklinkWebService.uploadInventoryImage(bricklinkSession, 171284300L, Paths.get("C:\\Users\\tvatt\\Desktop\\lego-collection-photos\\6658-1-f81eb8d9ddc469b48dff9c45064be5eb\\bl-inventory-photo.jpg"));
-        bricklinkSession = bricklinkWebService.logout(bricklinkSession);
+            BricklinkWebService bricklinkWebService = new BricklinkWebService(properties, mapper);
+            bricklinkSession = bricklinkWebService.authenticate();
+            bricklinkWebService.uploadInventoryImage(bricklinkSession, 171330559L, Paths.get("C:\\Users\\tvatt\\Desktop\\lego-collection-photos\\6658-1-f81eb8d9ddc469b48dff9c45064be5eb\\bl-inventory-photo.jpg"));
+            bricklinkSession = bricklinkWebService.logout(bricklinkSession);
+        } finally {
+            timer.stop();
+        }
+        log.info("uploaded photo [{}} to inventory [{}] in [{}] ms", "abc", 1234567L, timer.getTotalTimeMillis());
 
         assertThat(bricklinkSession).isNotNull();
     }
